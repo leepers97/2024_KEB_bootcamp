4f1528ced2116d6f7061b4a0a708f1dcf8847c09
hch <hch5362@gmail.com>
2024-01-15 15:47:31 +0900

    day2_5

--- day02.py
+++ day02.py
@@ -50,7 +50,8 @@ print(groups)
 
 
 
-# chapter3
+
+# chapter 3
 # 숫자
 
 # Bool : 0이 아닌 수는 모두 True
@@ -64,17 +65,108 @@ print(type(cash))
 
 
 
-# 거듭제곱
-base_number = int(input('Input base number : '))
-exponent_number = int(input('Input exponent number : '))
-# 1. f스트링
-print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
-print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+# # 거듭제곱
+# base_number = int(input('Input base number : '))
+# exponent_number = int(input('Input exponent number : '))
+# # 1. f스트링
+# print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
+# print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+#
+# # 2. format 함수
+# print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+# print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+#
+# # 3. c like
+# print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, pow(base_number, exponent_number)))
 
-# 2. format 함수
-print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
-print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
 
-# 3. c like
-print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, pow(base_number, exponent_number)))
 
+# # 몫, 나머지 한번에 튜플로
+# first_number = int(input("First number : "))
+# second_number = int(input("Second number : "))
+#
+# quotient = first_number // second_number
+# remainder = first_number % second_number
+# print(f'몫은 {quotient} 나머지는 {remainder}입니다')
+# print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다')
+
+
+
+# 다시 보자 +-
+print(-5 ** 2)
+print(-(5 ** 2))
+print((-5) ** 2)
+
+# 접두어 (다시보자 - 아스키코드 관련, 각각 몇진수인지)
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(binary), chr(octal), chr(hexadecimal), chr(binary)) # 아스키 코드로 변환
+print(bin(binary), bin(octal), bin(hexadecimal), chr(binary)) # 이진수로 변환
+print(ord('B'), ord('Z'), ord('a'), ord('2')) # 66, 90, 97, 50
+# 아스키코드 스타트지점들 외워두기
+
+
+
+# # 형변환
+# fahrenheit = float(input("Input Fahrenheit : "))
+# print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5 / 9) : .4f}C')
+
+
+
+print(int('10', 2)) # 2진수
+print(int('10', 8)) # 8진수
+print(int('10', 16)) # 16진수
+print(int('11', 16)) # 16진수
+print(int('1A', 16)) # 16진수
+print(int('10', 22)) # 22진수
+
+
+
+
+
+# chapter 4
+
+# menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
+# if menu == '1' :
+#     fahrenheit = float(input("Input Fahrenheit : "))
+#     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5 / 9) : .4f}C')
+# elif menu == '2' :
+#     celsius = float(input("Input Celsius : "))
+#     print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9 / 5) + 32) : .4f}C')
+# else :
+#     print("Terminate Program")
+
+
+
+# 줄바꿈
+sum = 1 + \
+    + 2 \
+    + 3
+print(sum)
+
+
+
+# None
+temp = []
+if temp :
+    print("원소가 존재하는 리스트")
+else:
+    print("비어있는 리스트")
+
+
+
+# 모음 자음
+letter = input('Input alphabet letter : ')
+
+# set 사용
+# vowels = {'a', 'e', 'i', 'o', 'u'}
+
+# 문자열 사용
+vowels = "aeiuo"
+if letter in vowels :
+    print(f'{letter} is a vowel')
+else :
+    print(f'{letter} is a consonant')
