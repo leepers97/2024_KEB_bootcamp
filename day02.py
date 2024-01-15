eec1563785ec8371283d28a9c8e0053e87093741
hch <hch5362@gmail.com>
2024-01-15 13:21:40 +0900

    day2_4

--- day02.py
+++ day02.py
@@ -54,3 +54,27 @@ print(groups)
 # 숫자
 
 # Bool : 0이 아닌 수는 모두 True
+
+# 콤마 사용 X (튜플로 인식)
+money = 5,000,000
+print(type(money))
+
+cash = 5_000_000 # _로 하면 가능
+print(type(cash))
+
+
+
+# 거듭제곱
+base_number = int(input('Input base number : '))
+exponent_number = int(input('Input exponent number : '))
+# 1. f스트링
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+
+# 2. format 함수
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+
+# 3. c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, pow(base_number, exponent_number)))
+
