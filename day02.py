6b7a373746e78368aaded1262b6ec549c1287d3f
hch <hch5362@gmail.com>
2024-01-15 10:56:21 +0900

    day2_2

--- day02.py
+++ day02.py
@@ -7,3 +7,19 @@ print(subjects[1])
 # list는 수정 가능(mutable)
 subjects[1] = "C++"
 print(subjects[1])
+
+# 리터럴 : 일반적으로 오른쪽에 위치
+
+# 변수(규칙 확인)
+# 대소문자 구분, 숫자로 시작X, 예약어를 변수로 설정X
+# case-sensitive
+abc = 7
+Abc = 8
+ABC = 6
+print(abc, Abc, ABC)
+
+test9 = 77
+#9test = 77
+_9test9 = 77
+
+#False = 123 # 예약어라 불가
