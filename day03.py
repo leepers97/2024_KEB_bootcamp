84c58ffd0ec31c2c6ced46bfe8176b8a7e1ed340
hch <hch5362@gmail.com>
2024-01-16 11:44:43 +0900

    day03_3

--- day03.py
+++ day03.py
@@ -16,6 +16,7 @@
         print("Wrong Input")'''
 
 
+# 초대코드 : vxrbjug
 
 # text strings
 # 아스키코드 보기 ex) 65 = a
@@ -32,7 +33,113 @@ print(university)
 
 # 문자 결합
 # +를 이용하여 결합 가능
-number1 = input("First Number : ")
-number2 = input("Second Number : ")
-print(number1 + number2) # int로 형변환하지 않아 문자열 결합
+#number1 = input("First Number : ")
+#number2 = input("Second Number : ")
+#print(number1 + number2) # int로 형변환하지 않아 문자열 결합
+
+# () 이용하여 결합 가능
+
+# *를 이용하여 문자열 반복 가능
+#print(number1 * 3)
+
+# []로 역방향 인덱싱 가능
+#print(number1[1])
+
+# 문자열은 immutable이므로 number[1] = 'P' 이런거 불가능
+# 하지만 replace로 교체 가능 (뭔가 틀렸는데 책보기 + 슬라이싱)
+name = 'Henny'
+name.replace('H', 'P')
+print(name)
+
+# slicing (자세히 알아보기)
+# [:] : 처음부터 끝까지
+# [start :] : start부터 끝까지
+# [: end] : 처음부터 end까지
+# [start : end] : start부터 end까지 -1
+# [start : end : step] : step만큼 증가
+univ = "InhaUniversity"
+print(univ[: 4])
+print(univ[:-10])
+print(univ[:len(univ)])
+
+# len : 문자열의 길이
+
+
+
+# split (중요함)
+# 문자열을 list로
+course = "2024 KEB Bootcamp"
+#list_course = course.split()
+list_course = course.split('B') # B가 구분자
+print(list_course)
+
+# number = input("FirstNumber SecondNumber : ").split()
+# print(number[0] + number[1]) # 결합
+# print(int(number[0]) + int(number[1])) # 산술 연산
+
+
+
+# join (중요함)
+# list를 문자열로
+subjects = ["python", "c++", "database"]
+subjects_string = " / ".join(subjects)
+print(subjects_string)
+
+
+# replace
+course2 = "KEB 2024 KEB Bootcamp"
+# print(course2)
+# print(course2.replace("KEB", "Inha"))
+# print(course2)
+# course2 = course2.replace("KEB", "Inha")
+# print(course2)
+
+print(course2)
+print(course2.replace("KEB", "Inha"))
+print(course2.replace("KEB", "Inha", 1))
+print(course2)
+
+
+
+# strip
+# strip : 양쪽 빈공간 삭제
+# lstrip : 왼쪽 빈공간 삭제
+# rstrip : 오른쪽 빈공간 삭제
+course3 = "* KEB 2024# KEB !Bootcamp KEB...*!#"
+print(course3.strip("!#.*")) # 양쪽 끝 연속적인 것만 건들임
+
+
+
+# 부록2 참고
+
+
+
+# 함수 몇 개
+# startswith('...') : ...으로 시작하는 문자열이 있으면 true값 반환
+# find('...') : ...의 index를 반환, 찾지 못하면 -1 반환
+print(course3.find("KEB"))
+print(course3.rfind("KEB")) # 뒤에서부터 찾기
+print(course3.index("KEB"))
+print(course3.find("Inha"))
+#print(course3.index("Inha")) # index는 찾지 못하면 예외 던짐(오류)
+
+subject = "python c++ database linux"
+subject2 = input("수강신청과목 입력 : ")
+# if subject.find(subject2) != -1:
+#     print(f'해당 과목은 존재합니다. 그리고 위치는 {subject.find(subject2)}번 인덱스입니다.')
+# else:
+#     print('해당 과목이 존재하지 않습니다')
+
+# index를 이용하면 존재하지 않을 때 오류남
+# if subject.index(subject2) != -1:
+#     print(f'해당 과목은 존재합니다. 그리고 위치는 {subject.index(subject2)}번 인덱스입니다.')
+# else:
+#     print('해당 과목이 존재하지 않습니다')
+
+# -> 예외처리 해주기
+try :
+    print(f'해당 과목은 존재합니다. 그리고 위치는 {subject.index(subject2)}번 인덱스입니다.')
+except ValueError :
+    print('해당 과목이 존재하지 않습니다')
+
 
